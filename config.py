cf70ba5a2d76206d71b999e1dbc61f975e72c3d6
gustavotorres-ipt <gustavotorres@ipt.br>
2026-05-26 10:36:51 -0300

    Changed config fil

--- config.py
+++ config.py
@@ -1,57 +1,11 @@
-<<<<<<< HEAD
-BATCH_SIZE = 256
-PATIENCE = 30
-EPOCHS = 32
-=======
 import torch
 
-BATCH_SIZE = 192
+BATCH_SIZE = 128
 PATIENCE = 10
 EPOCHS = 30
->>>>>>> real-seismic-clip
 PROJECTION_SIZE = 512
 IMG_SIZE = 96
 
-<<<<<<< HEAD
-# VISION_MODEL = 'checkpoints/resnet18_image_encoder.pth'
-# LANGUAGE_MODEL = 'checkpoints/seismic_distilbert.pt'
-# 
-# IMAGE_FOLDER_TRAIN = 'data/images/training'
-# TEXT_FOLDER_TRAIN = 'data/captions/training'
-# 
-# IMAGE_FOLDER_VAL = 'data/images/validation'
-# TEXT_FOLDER_VAL = 'data/captions/validation'
-# 
-# LEARNING_RATES = {
-#     'image_encoder': 5e-5,
-#     'text_encoder': 5e-5,
-#     'image_proj': 5e-4,
-#     'text_proj': 5e-4,
-#     'logit_scale': 5e-4
-# }
-# OUTPUT_MODEL = "clip_sismico_sintetico.pth"
-
-
-# VISION_MODEL = 'checkpoints/resnet18_inline_xline_todos_tams_balanceado_encoder.pth'
-# LANGUAGE_MODEL = 'checkpoints/mlm_sismofacies.pt'
-# 
-# IMAGE_FOLDER_TRAIN = 'data/janelas_iline_xline_32_40_64_balanceado/training'
-# TEXT_FOLDER_TRAIN = 'data/legendas_iline_xline_32_40_64_balanceado/training'
-#  
-# IMAGE_FOLDER_VAL = 'data/janelas_iline_xline_32_40_64_balanceado/validation'
-# TEXT_FOLDER_VAL = 'data/legendas_iline_xline_32_40_64_balanceado/validation'
-# 
-# LEARNING_RATES = {
-#     'image_encoder': 1e-5,
-#     'text_encoder':  1e-5,
-#     'image_proj':    1e-4,
-#     'text_proj':     1e-4,
-#     'logit_scale':   1e-4
-# }
-# OUTPUT_MODEL = "clip_il_xl_32_40_64_balanceado.pth"
-
-=======
->>>>>>> real-seismic-clip
 VISION_MODEL = 'checkpoints/resnet18_parihaka_f3_encoder.pth'
 LANGUAGE_MODEL = 'checkpoints/lang_ckpt_parihaka_f3.pt'
 
@@ -70,8 +24,7 @@ LEARNING_RATES = {
     'text_proj':     1e-4,
     'logit_scale':   1e-4
 }
-# CLIP_FILE = 'clip_parihaka_f3.pth'
-CLIP_FILE = 'aaa.pth'
+CLIP_FILE = 'clip_parihaka_f3.pth'
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
